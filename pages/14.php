bd056eda0efd949ac4256c54da793ea123679a91
Gecko <songecko@gmail.com>
2013-07-07 16:32:48 -0300

    -

--- pages/14.php
+++ pages/14.php
@@ -1,7 +1,7 @@
-<div class="pt-page pt-page-i-10 container-fluid greenSection">
+<div class="pt-page pt-page-i-10 container-fluid blueSection">
 				<p class="pageTitleHolder">Objetivos</p>
 				<audio preload="auto"><source src="audio/03_OBJETIVO.mp3" type="audio/mpeg" /></audio>
-				<div class="contenido nextButton text-center">
+				<div class="contenido nextButton text-center" data-go-to-page="4">
                     <p>Durante este entrenamiento serás capacitado para conocer las caracteristicas de tu tienda, cuál es el portafolio P&G óptimo por tipo de tienda, cuáles son los exhibidores que ayudan a la visibilidad de las marcas, así como todos los pasos que debes tomar en cuenta para realizar una venta efectiva.
 Al final de este entrenamiento accederás a una autoevaluación para que puedas ver que tanto has aprendido.</p>
 					<p class="textoDestacado">Bienvenido y Mucho Éxito!</p>
