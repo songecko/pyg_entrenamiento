f959c0f56205ca59b86aff6180e8cb5f78a3e1c0
Gecko <songecko@gmail.com>
2013-07-07 16:54:44 -0300

    -

--- pages/15.php
+++ pages/15.php
@@ -1,12 +1,15 @@
-<div class="pt-page pt-page-dp-s-1 container-fluid text-center">				
-            	<img src="img/fondo_2.jpg" class="background" />
-				<div class="SeccionPosicion content">
-					<p class="tituloSeccion">Descripción del Puesto</p>
-                    <ul>
-                    	<li class="posicion supervisor nextButton">
-                        	<img src="img/foto-01.jpg"  />
-                            <div class="titulo"><p>Supervisor</p><img src="img/bt-siguiente.png" class="nextPosicion" /></div>
-                        </li>
-                    </ul>
+<div class="pt-page pt-page-dp container-fluid text-center">				
+	<img src="img/fondo_2.jpg" class="background" />
+	<div class="SeccionPosicion content">
+		<p class="tituloSeccion">Descripción del Puesto</p>
+		<ul>
+			<li class="posicion supervisor nextButton">
+				<img src="img/foto-01.jpg"  />
+				<div class="titulo">
+					<p>Supervisor</p>
+					<img src="img/bt-siguiente.png" class="nextPosicion" />
 				</div>
-			</div>
+			</li>
+		</ul>
+	</div>
+</div>
--- templates/header.php
+++ templates/header.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="apple-mobile-web-app-capable" content="yes" />
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
 		<!-- CSS -->
 		<link href="css/bootstrap.min.css" rel="stylesheet" media="screen">
