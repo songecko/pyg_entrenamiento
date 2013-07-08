2d1cdf000f28e8797c383f80aebbddccfd50d637
Patiz <pbmaidana@gmail.com>
2013-07-07 21:48:39 -0300

    cambios generales

--- pages/72.php
+++ pages/72.php
@@ -1,6 +1,6 @@
 <div class="pt-page pt-page-vbc-5 container-fluid orangeSection">
 	<p class="pageTitleHolder">Indicadores de Desempeño</p>
-	<div class="contenido nextButton" data-go-to-page="19">
+	<div class="contenido nextButton" data-go-to-page="4">
 		<img src="http://placehold.it/744x400">
 	</div>
 </div>
